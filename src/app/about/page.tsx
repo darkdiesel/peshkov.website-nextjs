1bfcbf3fd8cc538f05dd8cbeca6a0d67885e44c1
Igor Peshkov <igor.peshkov@gmail.com>
2024-06-12 17:46:20 +0300

    Install bootstrap. Add About empty page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from 'react-bootstrap';
+
+export default function Page() {
+    return <>
+        <h1>About page</h1>
+        <div>
+            <Button value={'Value'} title={'Title'}/>
+        </div>
+    </>
+}
